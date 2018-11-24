2f22292c33757ec9d450694ccac002cdc71d9e4d
Artsiom Mezin <iketari@gmail.com>
2018-11-24 19:00:50 +0000

    visual part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ResultPanelComponent } from './components/result-panel/result-panel.component';
+import { ItemsTableComponent } from './components/items-table/items-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ResultPanelComponent,
+    ItemsTableComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/items-table/items-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-items-table',
+  templateUrl: './items-table.component.html',
+  styleUrls: ['./items-table.component.scss']
+})
+export class ItemsTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
